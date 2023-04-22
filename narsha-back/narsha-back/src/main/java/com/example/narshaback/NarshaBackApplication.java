385e44fa6f5cee63224de8b90c7cc0fb40fa5658
yxxyn20 <dbtndus0527@gmail.com>
2023-04-22 22:29:27 +0900

    [init] spring boot setting 스프링 부트 서버 기본 세팅, mysql 연결

---
+++ narsha-back/narsha-back/src/main/java/com/example/narshaback/NarshaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.narshaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarshaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarshaBackApplication.class, args);
+	}
+
+}
---
+++ narsha-back/narsha-back/src/test/java/com/example/narshaback/NarshaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.narshaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarshaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
