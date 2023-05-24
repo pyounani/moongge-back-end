389f3b6c75dcf3f7f883287e84d1c9f957d2b221
yxxyn20 <dbtndus0527@gmail.com>
2023-05-25 00:25:25 +0900

    [feat] S3에 1개의 이미지를 등록하는 API 작성 - S3에 1개의 이미지를 등록하는 API 작성

--- narsha-back/narsha-back/src/main/java/com/example/narshaback/service/AmazonS3Service.java
+++ narsha-back/narsha-back/src/main/java/com/example/narshaback/service/AmazonS3Service.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public interface AmazonS3Service {
     List<S3FileDTO> uploadFiles(String fileType, List<MultipartFile> multipartFiles); // n개의 파일 업로드
+
+    S3FileDTO uploadFile(MultipartFile multipartFile); // 1개의 파일 업로드
+
     String deleteFile(String uploadFilePath, String uuidFileName); // 파일 삭제
     String getUuidFileName(String fileName); // uuid 파일명 변환
     String getFolderName(); // 년월일 폴더명 변환
--- narsha-back/narsha-back/src/main/java/com/example/narshaback/service/AmazonS3ServiceImpl.java
+++ narsha-back/narsha-back/src/main/java/com/example/narshaback/service/AmazonS3ServiceImpl.java
@@ -76,6 +76,52 @@ public class AmazonS3ServiceImpl implements AmazonS3Service{
     }
 
     @Override
+    public S3FileDTO uploadFile(MultipartFile multipartFile) {
+
+        String uploadFilePath = "profile" + "/" + getFolderName();
+
+
+        String originalFileName = multipartFile.getOriginalFilename();
+        String uploadFileName = getUuidFileName(originalFileName);
+        String uploadFileUrl = "";
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(multipartFile.getSize());
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+
+            String keyName = uploadFilePath + "/" + uploadFileName; // ex) 구분/년/월/일/파일.확장자
+
+            // S3에 폴더 및 파일 업로드
+            amazonS3Client.putObject(
+                    new PutObjectRequest(bucketName, keyName, inputStream, objectMetadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead));
+
+    /*amazonS3Client.putObject(
+        new PutObjectRequest(bucket, s3Key, inputStream, objectMetadata)
+            .withCannedAcl(CannedAccessControlList.PublicRead));*/
+
+            // S3에 업로드한 폴더 및 파일 URL
+            uploadFileUrl = amazonS3Client.getUrl(bucketName, keyName).toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("Filed upload failed", e);
+        }
+
+        S3FileDTO s3files = S3FileDTO.builder()
+                        .originalFileName(originalFileName)
+                        .uploadFileName(uploadFileName)
+                        .uploadFilePath(uploadFilePath)
+                        .uploadFileUrl(uploadFileUrl)
+                        .build();
+
+
+        return s3files;
+    }
+
+    @Override
     public String deleteFile(String uploadFilePath, String uuidFileName) {
         String result = "success";
 
