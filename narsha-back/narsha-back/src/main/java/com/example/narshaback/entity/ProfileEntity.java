6041f70684bfd7fd3e7fbec19126812bfb364776
yxxyn20 <dbtndus0527@gmail.com>
2023-05-24 22:25:21 +0900

    [feat] Profile Entity 생성 및 User Entity 수정 - 여러 그룹에 속한 1명의 유저가 n개의 프로필을 가지고 있어야 함. - 유저-그룹 : 프로필 = 1:1 관계 - Profile Entity 생성, User Entity에 있던 속성(닉네임, 생일, 뱃지 리스트, 프로필 소개, 프로필 이미지 링크)을 Profile Entity로 옮김

---
+++ narsha-back/narsha-back/src/main/java/com/example/narshaback/entity/ProfileEntity.java
@@ -0,0 +1,35 @@
+package com.example.narshaback.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Builder // DTO -> Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; // 프로필 고유 id
+
+    @OneToOne
+    private User_Group user_group; // 유저-그룹 id
+
+    @Column(nullable = false, length=20)
+    private String nikname; // 닉네임
+
+    @Column(length=200)
+    private String profileImage; // 프로필 이미지 링크
+
+    @Column(length=20)
+    private String birth; // 생일: ex) 2023.05.08
+
+    @Column(length=100)
+    private String intro; // 프로필 소개
+
+    @Column(length=500)
+    private String badgeList; // 뱃지 리스트(string array)
+}
--- narsha-back/narsha-back/src/main/java/com/example/narshaback/entity/UserEntity.java
+++ narsha-back/narsha-back/src/main/java/com/example/narshaback/entity/UserEntity.java
@@ -26,17 +26,4 @@ public class UserEntity {
 
     @Column(nullable = false, length=20)
     private String name; // 이름
-
-    @Column(nullable = false, length=20)
-    private String nikname; // 닉네임
-
-    @Column(length=500)
-    private String badgeList; // 뱃지 리스트(string array)
-
-    @Column(length=200)
-    private String profileImage; // 프로필 이미지 링크
-
-    @Column(length=20)
-    private String birth; // 생일: ex) 2023.05.08
-
 }
