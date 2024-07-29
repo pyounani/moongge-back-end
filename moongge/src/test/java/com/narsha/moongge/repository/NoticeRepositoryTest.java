dc4c60d242a50c7fb1b3ac01939f1fa4fea9b04a
pyounani <yndbsk9372@gmail.com>
2024-07-29 14:20:58 +0900

    Refactor: 공지 리포지토리 테스트 리팩토링

--- moongge/src/test/java/com/narsha/moongge/repository/NoticeRepositoryTest.java
+++ moongge/src/test/java/com/narsha/moongge/repository/NoticeRepositoryTest.java
@@ -3,12 +3,6 @@ package com.narsha.moongge.repository;
 import com.narsha.moongge.entity.GroupEntity;
 import com.narsha.moongge.entity.NoticeEntity;
 import com.narsha.moongge.entity.UserEntity;
-import com.narsha.moongge.base.dto.group.CreateGroupDTO;
-import com.narsha.moongge.base.dto.notice.CreateNoticeDTO;
-import com.narsha.moongge.base.dto.user.UserRegisterDTO;
-import com.narsha.moongge.service.GroupService;
-import com.narsha.moongge.service.NoticeService;
-import com.narsha.moongge.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,17 +17,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class NoticeRepositoryTest {
 
     @Autowired
-    private GroupService groupService;
-    @Autowired
     private UserRepository userRepository;
     @Autowired
     private GroupRepository groupRepository;
     @Autowired
     private NoticeRepository noticeRepository;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private NoticeService noticeService;
 
     @Test
     void 최신_공지_불러오기() {
@@ -60,15 +48,6 @@ class NoticeRepositoryTest {
         assertEquals(group.getGroupCode(), latestNotice.getGroup().getGroupCode(), "최신 공지 그룹 코드가 일치해야 합니다.");
     }
 
-    private CreateNoticeDTO buildCreateNoticeDTO(UserEntity user, GroupEntity group, String title, String content) {
-        return CreateNoticeDTO.builder()
-                .groupCode(group.getGroupCode())
-                .noticeTitle(title)
-                .noticeContent(content)
-                .writer(user.getUserId())
-                .build();
-    }
-
     private NoticeEntity createNotice(UserEntity user, GroupEntity group, String title, String content) {
         return NoticeEntity.builder()
                 .user(user)
@@ -79,33 +58,29 @@ class NoticeRepositoryTest {
     }
 
     private GroupEntity createGroup(UserEntity user) {
-        CreateGroupDTO createGroupDTO = CreateGroupDTO.builder()
+        GroupEntity group = GroupEntity.builder()
+                .groupCode("groupCode")
                 .groupName("groupName")
                 .school("school")
                 .grade(3)
                 .groupClass(5)
-                .userId(user.getUserId())
                 .build();
 
-        groupService.createGroup(createGroupDTO);
+        GroupEntity savedGroup = groupRepository.save(group);
 
-        Optional<GroupEntity> savedGroup = groupRepository.findByGroupCode(user.getGroup().getGroupCode());
-        assertTrue(savedGroup.isPresent());
-        return savedGroup.get();
+        user.setGroup(savedGroup);
+
+        return savedGroup;
     }
 
     private UserEntity createUser() {
-        UserRegisterDTO userRegisterDTO = UserRegisterDTO.builder()
+        UserEntity user = UserEntity.builder()
                 .userId("userId")
                 .userType("teacher")
                 .password("password")
-                .name("name")
+                .userName("name")
                 .build();
 
-        userService.register(userRegisterDTO);
-
-        Optional<UserEntity> savedUser = userRepository.findByUserId("userId");
-        assertTrue(savedUser.isPresent(), "유저가 저장되어 있어야 합니다.");
-        return savedUser.get();
+        return userRepository.save(user);
     }
 }
