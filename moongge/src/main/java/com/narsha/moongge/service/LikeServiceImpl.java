a27d7223f45e28712493eb3a4a90faf1f6cab4d2
pyounani <yndbsk9372@gmail.com>
2024-07-30 09:43:38 +0900

    Refactor: 좋아요 생성하기 서비스 리팩토링

--- moongge/src/main/java/com/narsha/moongge/service/LikeServiceImpl.java
+++ moongge/src/main/java/com/narsha/moongge/service/LikeServiceImpl.java
@@ -14,7 +14,6 @@ import com.narsha.moongge.repository.GroupRepository;
 import com.narsha.moongge.repository.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,43 +25,35 @@ import java.util.Optional;
 public class LikeServiceImpl implements LikeService{
 
     private final LikeRepository likeRepository;
-
     private final PostRepository postRepository;
     private final GroupRepository groupRepository;
-
     private final UserRepository userRepository;
     private final AlarmRepository alarmRepository;
 
-    private final ApplicationEventPublisher eventPublisher;
-
+    /**
+     * 좋아요 생성하기
+     */
     @Override
     public Integer createLike(String groupCode, Integer postId, CreateLikeDTO createLikeDTO) {
 
-        Optional<UserEntity> user = userRepository.findByUserId(createLikeDTO.getUserId());
-        if(!user.isPresent()) {
-            throw new UserNotFoundException(ErrorCode.USERID_NOT_FOUND);
-        }
+        GroupEntity group = groupRepository.findByGroupCode(groupCode)
+                .orElseThrow(() -> new GroupNotFoundException(ErrorCode.GROUP_NOT_FOUND));
 
-        Optional<PostEntity> post = postRepository.findByPostId(createLikeDTO.getPostId());
-        // 게시물이 존재하지 않은 경우
-        if (!post.isPresent()) {
-            throw new PostNotFoundException(ErrorCode.POST_NOT_FOUND);
-        }
+        PostEntity post = postRepository.findByPostIdAndGroup(postId, group)
+                .orElseThrow(() -> new PostNotFoundException(ErrorCode.POST_NOT_FOUND));
 
-        Optional<GroupEntity> group = groupRepository.findByGroupCode(createLikeDTO.getGroupCode());
-        if(!group.isPresent()){
-            throw new GroupNotFoundException(ErrorCode.GROUP_NOT_FOUND);
-        }
+        UserEntity user = userRepository.findByUserId(createLikeDTO.getUserId())
+                .orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND));
 
         LikeEntity like = LikeEntity.builder()
-                .post(post.get())
-                .group(group.get())
-                .user(user.get())
+                .group(group)
+                .post(post)
+                .user(user)
                 .build();
 
-        likeRepository.save(like);
+        LikeEntity savedLike = likeRepository.save(like);
 
-        return likeRepository.save(like).getLikeId();
+        return savedLike.getLikeId();
     }
 
 
