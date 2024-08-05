63cd565d381e95d39c77341603b5784b73175346
pyounani <yndbsk9372@gmail.com>
2024-08-05 15:47:10 +0900

    Fix: JPA N+1 쿼리 문제 수정

--- moongge/src/test/java/com/narsha/moongge/repository/PostRepositoryTest.java
+++ moongge/src/test/java/com/narsha/moongge/repository/PostRepositoryTest.java
@@ -50,7 +50,7 @@ class PostRepositoryTest {
         LocalDateTime endTime = now.plusDays(1);
 
         // when
-        List<PostEntity> posts = postRepository.findByGroupAndCreateAtBetweenOrderByCreateAtDesc(group, startTime, endTime);
+        List<PostEntity> posts = postRepository.findPostsWithUserAndGroup(group, startTime, endTime);
 
         // then
         assertEquals(3, posts.size(), "조회된 포스트 수가 일치해야 합니다.");
