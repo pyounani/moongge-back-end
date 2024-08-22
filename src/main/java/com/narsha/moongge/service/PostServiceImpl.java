5adf2e1f208222e1945b2d3ad06cb8723f8cc356
pyounani <yndbsk9372@gmail.com>
2024-08-22 18:39:49 +0900

    Fix: 게시물 목록 시간 수정

--- src/main/java/com/narsha/moongge/service/PostServiceImpl.java
+++ src/main/java/com/narsha/moongge/service/PostServiceImpl.java
@@ -13,6 +13,7 @@ import com.narsha.moongge.repository.LikeRepository;
 import com.narsha.moongge.repository.PostRepository;
 import com.narsha.moongge.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,6 +26,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class PostServiceImpl implements PostService {
 
     private static final String POST_PHOTOS_FOLDER = "post/photos";
