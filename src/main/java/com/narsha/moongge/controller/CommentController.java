803441642f00e4196624ba5026f47c45c7289877
pyounani <yndbsk9372@gmail.com>
2024-08-31 10:46:54 +0900

    Feat: Swagger 문서에 에러 응답 메시지 추가

--- src/main/java/com/narsha/moongge/controller/CommentController.java
+++ src/main/java/com/narsha/moongge/controller/CommentController.java
@@ -42,7 +42,9 @@ public class CommentController {
             },
             responses = {
                     @ApiResponse(responseCode = "200", description = "댓글 작성을 성공했습니다.", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json")),
-                    @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json"))
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "404", description = "아이디에 해당하는 유저를 찾을 수 없습니다.", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "404", description = "게시글 정보를 찾을 수 없습니다.", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json"))
             }
     )
     public ResponseEntity<ResponseDTO> createComment(@PathVariable String userId,
